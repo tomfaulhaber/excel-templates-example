eeece7ae93be5394e40cde56b6651d4c08dcb61a
Tom Faulhaber <git_com@infolace.com>
2016-02-22 10:59:35 -0800

    Update to excel-template 0.3.1

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "1.0.1"]
                  [clj-time "0.9.0"]
-                 [com.infolace/excel-templates "0.3.0-SNAPSHOT"]
+                 [com.infolace/excel-templates "0.3.1"]
                  [org.clojure/clojure "1.6.0"]])
